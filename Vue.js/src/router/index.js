afd138ad0ac5bc94eb67ac720938cecf6361f445
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-05-03 00:37:40 -0300

    Update index.js

--- Vue.js/src/router/index.js
+++ Vue.js/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import TheContainer from '@/components/TheContainer.vue'
 import TheInstructions from '@/components/TheInstructions.vue'
 import TheContactForm from '@/components/TheContactForm.vue'
+import NotFound from '@/components/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,19 +19,20 @@ const router = createRouter({
       props: true,
       children: [
         {
-          // UserProfile will be rendered inside User's <router-view>
-          // when /user/:id/profile is matched
           path: 'about',
-          component: TheInstructions,
+          component: TheInstructions
         },
         {
-          // UserPosts will be rendered inside User's <router-view>
-          // when /user/:id/posts is matched
           path: 'contact',
-          component: TheContactForm,
+          component: TheContactForm
         },
-      ],
-    },
+        {
+          path: '/:pathMatch(.*)*',
+          name: 'NotFound',
+          component: NotFound
+        }
+      ]
+    }
   ]
 })
 

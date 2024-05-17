9a99b0a94cfb9cbe2d1f288ee5aef51311ef657c
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-05-17 00:17:42 -0300

    Add files via upload

---
+++ React.js+Next.js/src/app/(content)/search/page.js
@@ -0,0 +1,15 @@
+import TheSearch from "@/components/TheSearch";
+import SearchEffects from "./effects";
+
+export const metadata = {
+  title: 'Sobre',
+};
+
+export default function About() {
+  return (
+    <>
+      <SearchEffects />
+      <TheSearch />
+    </>
+  );
+}

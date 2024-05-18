9c226dbbac9ecb2399bd726614ecde05ec82ed0b
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-05-18 00:57:23 -0300

    Update TheHeader.vue
    Mecanismo de busca funcionando!

--- Vue.js+Vite.js/src/components/TheHeader.vue
+++ Vue.js+Vite.js/src/components/TheHeader.vue
@@ -5,7 +5,7 @@
     <RouterLink to="/"><div id="simbolo"></div></RouterLink>
     <RouterLink to="/"><div id="logotipo"></div></RouterLink>
     <form id="buscaAmplo" action="/search" method="get" role="search">
-      <div id="digitar"><input type="text" placeholder="buscar já!" name="japa" /></div>
+      <div id="digitar"><input type="text" placeholder="buscar já!" name="q" /></div>
       <input type="submit" id="pesquisa" value="" />
     </form>
     <RouterLink to="/nearby"><div id="nearby" alt="Por Perto"></div></RouterLink>
@@ -13,7 +13,7 @@
   </header>
   <header id="reduzido">
     <form id="buscaReduz" action="/search" method="get" role="search">
-      <div id="digitar"><input type="text" placeholder="buscar já!" name="japa" /></div>
+      <div id="digitar"><input type="text" placeholder="buscar já!" name="q" /></div>
       <input type="submit" id="pesquisa" value="" />
     </form>
   </header>

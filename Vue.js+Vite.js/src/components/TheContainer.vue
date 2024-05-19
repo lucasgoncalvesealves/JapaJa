a8c988fc19ab18d84918fbe7f6c787f6a84f59a4
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-05-19 19:52:17 -0300

    Add files via upload

--- Vue.js+Vite.js/src/components/TheContainer.vue
+++ Vue.js+Vite.js/src/components/TheContainer.vue
@@ -15,35 +15,6 @@
         margin: 0 auto 1em auto;
     }
 }
-#subheader {
-    width: 100%;
-    margin-bottom: 40px;
-    display: flex; 
-    justify-content: space-between;
-}
-#voltar {
-    width: 82px; 
-    height: 30px; 
-    background-image: url(../assets/voltar.png); 
-    background-size: cover; 
-    transition: .25s;
-    &:hover {
-        background-image: url(../assets/voltar_on.png);
-    }
-}
-
-/* incorporando a fonte Gala Bold */
-@font-face 
-{
-    font-family: 'GalaBold';
-    src: url('../assets/font/gala_bold-webfont.eot');
-    src: url('../assets/font/gala_bold-webfont.eot?#iefix') format('embedded-opentype'),
-         url('../assets/font/gala_bold-webfont.woff') format('woff'),
-         url('../assets/font/gala_bold-webfont.ttf') format('truetype'),
-         url('../assets/font/gala_bold-webfont.svg#GalaRegular') format('svg');
-    font-weight: normal;
-    font-style: normal;
-}
 
 /* design responsivo */
 @media only screen and (min-width: 414px)

3c2c6ad003112a46d0a2b08596f0562082900c7d
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-05-19 19:48:07 -0300

    Add files via upload
    Remodelagem da versão mobile.

--- Vue.js+Vite.js/src/components/TheSubheader.vue
+++ Vue.js+Vite.js/src/components/TheSubheader.vue
@@ -43,4 +43,17 @@
             background-image: url(../assets/voltar_on.png);
         }
     }
+
+    /* incorporando a fonte Gala Bold */
+    @font-face 
+    {
+        font-family: 'GalaBold';
+        src: url('../assets/font/gala_bold-webfont.eot');
+        src: url('../assets/font/gala_bold-webfont.eot?#iefix') format('embedded-opentype'),
+            url('../assets/font/gala_bold-webfont.woff') format('woff'),
+            url('../assets/font/gala_bold-webfont.ttf') format('truetype'),
+            url('../assets/font/gala_bold-webfont.svg#GalaRegular') format('svg');
+        font-weight: normal;
+        font-style: normal;
+    }
 </style>

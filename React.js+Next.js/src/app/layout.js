2f702489b14a02878b1386a9f7d57e8ce7d9184d
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-05-19 21:17:06 -0300

    Add files via upload
    Remodelagem para a versão mobile.

--- React.js+Next.js/src/app/layout.js
+++ React.js+Next.js/src/app/layout.js
@@ -13,10 +13,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <TheHeader />
-        {children}
-        <script async src="https://cse.google.com/cse.js?cx=d38731f1cf69b44da"></script>
-        <TheFooter />
+        <div id="wrapper">
+          <TheHeader />
+          {children}
+          <script async src="https://cse.google.com/cse.js?cx=d38731f1cf69b44da"></script>
+          <TheFooter />
+        </div>
       </body>
     </html>
   );
--- React.js+Next.js/src/app/not-found.js
+++ React.js+Next.js/src/app/not-found.js
@@ -1,93 +1,108 @@
 'use client'
 
+import TheSubheader from "@/components/TheSubheader";
 import Error404 from "@/components/Error404";
 
 export default function NotFound() {
   return (
     <>
-        <div id="conteudo">
-            <div id="subheader">
-            <h1>Erro 404</h1>
-            <a id="voltar" href="javascript:history.go(-1)"></a>
-            </div>
+        <div id="container">
+            <TheSubheader title="Erro 404"/>
             <Error404 />
         </div>
 
         <style jsx>{`
-            #conteudo {
-                height: auto;
-                padding: 20px;
-                text-align: left;
-                margin-top: 7px;
-                background-color: #fff;
-                border: 3px solid #f63;
-                border-radius: 7px;
+            #container {
+                height: auto; 
+                text-align: left; 
+                background-color: #FFF; 
                 transition: .25s;
-                &:hover {
-                    border: 3px solid #f99;
-                }
                 & p {
                     font: 14px 'Trebuchet MS';
+                    margin: 0 auto 1em auto;
                 }
-                & form {
-                    margin-top: 40px;
+            }
+            h1 {
+                font: normal 40px 'GalaBold'; 
+                float: left; 
+                width: auto; 
+                color: #F63; 
+                padding-right: 20px; 
+                margin: 0; 
+                transition: .25s;
+                &:hover {
+                    color: #F99;
                 }
             }
             #subheader {
                 width: 100%;
-                height: 73px;
-                margin-bottom: 15px;
-                display: flex;
+                margin-bottom: 40px;
+                display: flex; 
                 justify-content: space-between;
             }
-            h1 {
-                font: normal 40px 'GalaBold';
-                width: auto;
-                color: #f63;
-                padding-right: 20px;
-                margin: 0;
-                transition: .25s;
-                &:hover {
-                    color: #f99;
-                }
-            }
             #voltar {
-                background-image: url(/voltar.png);
-                background-size: cover;
-                width: 82px;
-                height: 30px;
+                width: 82px; 
+                height: 30px; 
+                background-image: url(/voltar.png); 
+                background-size: cover; 
                 transition: .25s;
                 &:hover {
                     background-image: url(/voltar_on.png);
                 }
             }
-            
-            /* incorporando a fonte Gala */
-            @font-face {
-            font-family: 'GalaBold';
-            src: url('/font/gala_bold-webfont.eot');
-            src:
-                url('/font/gala_bold-webfont.eot?#iefix') format('embedded-opentype'),
-                url('/font/gala_bold-webfont.woff') format('woff'),
-                url('/font/gala_bold-webfont.ttf') format('truetype'),
-                url('/font/gala_bold-webfont.svg#GalaRegular') format('svg');
-            font-weight: normal;
-            font-style: normal;
+            /* incorporando a fonte Gala Bold */
+            @font-face 
+            {
+                font-family: 'GalaBold';
+                src: url(/font/gala_bold-webfont.eot);
+                src: url(/font/gala_bold-webfont.eot?#iefix) format('embedded-opentype'),
+                     url(/font/gala_bold-webfont.woff) format('woff'),
+                     url(/font/gala_bold-webfont.ttf) format('truetype'),
+                     url(/font/gala_bold-webfont.svg#GalaRegular) format('svg');
+                font-weight: normal;
+                font-style: normal;
             }
-            
             /* design responsivo */
-            @media only screen and (min-width: 414px) {
-                #conteudo {
+            @media only screen and (min-width: 414px)
+            {
+                #container {
                     width: 362px;
+                    border: 3px solid #F63; 
+                    border-radius: 7px;
+                    overflow: auto;
+                    height: 100%;
+                    margin: 96px 0 0 7px;
+                    padding: 20px;
+                    &:hover {
+                        border: 3px solid #F99;
+                    }
+                    &::-webkit-scrollbar {
+                        width: 10px;
+                    }
+                    &::-webkit-scrollbar-track {
+                        border-radius: 10px;
+                        background: #FFF;
+                    } 
+                    &::-webkit-scrollbar-thumb {
+                        border-radius: 10px;
+                        background: #CCC;
+                    }
+                    &::-webkit-scrollbar-thumb:hover {
+                        background: #AAA; 
+                    }
                 }
             }
-            @media only screen and (min-width: 863px) {
-                #conteudo {
-                    width: 778px;
+            @media only screen and (min-width: 863px)
+            {
+                #container {
+                    width: 777px;
+                    height: auto;
+                    margin: 7px 0 0 0;
                 }
             }
-            @media only screen and (min-width: 1270px) {
-                #conteudo {
+            @media only screen and (min-width: 1270px)
+            {
+                #container {
                     width: 1193px;
                 }
             }

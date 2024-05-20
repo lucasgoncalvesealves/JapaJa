df756e84a855a39015841fe0b8931dc4466915d9
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-05-20 07:11:10 -0300

    Update TheNearby.jsx
    Implementação do componente e da função Por Perto.

--- React.js+Next.js/src/components/TheNearby.jsx
+++ React.js+Next.js/src/components/TheNearby.jsx
@@ -6,8 +6,26 @@ export default function TheNearby()
 {
   return (
     <>
-      <div class="gcse-searchresults-only"></div>
-      <script async src="https://cse.google.com/cse.js?cx=d38731f1cf69b44da"></script>
+      <div id="mapa">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d14698.842363543252!2d-43.20081595!3d-22.924043150000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1srestaurante%20japon%C3%AAs!5e0!3m2!1spt-BR!2sbr!4v1716193642248!5m2!1spt-BR!2sbr" width="800" height="600" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+
+      <style jsx>{`
+        #mapa {
+          padding-bottom: 79%;
+          position: relative;
+          height: 0;
+          overflow: hidden;
+          & iframe {
+              left: 0;
+              top: 0;
+              border: 0;
+              height: 100%;
+              width: 100%;
+              position: absolute;
+          }
+        }
+      `}</style>
     </>
   )
 }

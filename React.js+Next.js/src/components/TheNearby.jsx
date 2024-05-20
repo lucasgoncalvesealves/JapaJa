3dac90530415a3f528ec4853bcbbec58a9c929eb
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-05-20 00:08:24 -0300

    Script de pesquisa do Google movido para seus devidos componentes.

--- React.js+Next.js/src/components/TheNearby.jsx
+++ React.js+Next.js/src/components/TheNearby.jsx
@@ -7,6 +7,7 @@ export default function TheNearby()
   return (
     <>
       <div class="gcse-searchresults-only"></div>
+      <script async src="https://cse.google.com/cse.js?cx=d38731f1cf69b44da"></script>
     </>
   )
 }

1748a36c886408013167b0af5095485169652cc1
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-11-18 06:04:18 -0300

    Update the-header.component.ts

--- Angular/src/app/the-header/the-header.component.ts
+++ Angular/src/app/the-header/the-header.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ButtonHomeComponent } from '../button-home/button-home.component';
+import { ButtonLogoComponent } from '../button-logo/button-logo.component';
+import { ButtonFormComponent } from '../button-form/button-form.component';
+import { ButtonNearbyComponent } from '../button-nearby/button-nearby.component';
+import { SearchBarComponent } from '../search-bar/search-bar.component';
 
 @Component({
   selector: 'app-the-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [ButtonHomeComponent, ButtonLogoComponent, ButtonFormComponent, ButtonNearbyComponent, SearchBarComponent],
   templateUrl: './the-header.component.html',
   styleUrl: './the-header.component.css'
 })

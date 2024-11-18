d5b7b2af692f9114031a6b87ddc7ef0cc71d643f
Gonçalves Alves <48800908+lucasgoncalvesealves@users.noreply.github.com>
2024-11-18 06:07:04 -0300

    Add files via upload

--- Angular/src/app/the-footer/the-footer.component.ts
+++ Angular/src/app/the-footer/the-footer.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonHomeComponent } from '../button-home/button-home.component';
+import { ButtonFormComponent } from '../button-form/button-form.component';
+import { ButtonAboutComponent } from '../button-about/button-about.component';
+import { ButtonNearbyComponent } from '../button-nearby/button-nearby.component';
 
 @Component({
   selector: 'app-the-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonAboutComponent, ButtonFormComponent, ButtonHomeComponent, ButtonNearbyComponent],
   templateUrl: './the-footer.component.html',
   styleUrl: './the-footer.component.css'
 })
